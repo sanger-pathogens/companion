e2e3de53521e79601ef7ec83234d3b4630e55008
Sascha Steinbiss <ss34@sanger.ac.uk>
2015-02-16 14:23:28 +0000

    use testing

---
+++ test/travis.setup-gt.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+wget https://github.com/genometools/genometools/archive/master.zip
+unzip master.zip
+cd genometools-master
+make -j3 cairo=no curses=no
+make -j3 cairo=no curses=no install
+cd ..
+rm -rf /opt/genometools-master*
