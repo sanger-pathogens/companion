c711ce3edeff5a95054859f1a3790211af4d5203
Sascha Steinbiss <ss34@sanger.ac.uk>
2015-02-16 17:07:23 +0000

    fix aragorn build script

--- test/travis.setup-aragorn.sh
+++ test/travis.setup-aragorn.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -ev
 wget http://mbio-serv2.mbioekol.lu.se/ARAGORN/Downloads/aragorn1.2.36.tgz
-tar -xvf aragorn1.2.36.tgz
-mv aragorn1* aragorn
+tar -xvf aragorn*.tgz
+rm aragorn*.tgz
+mv aragorn* aragorn
 cd aragorn
 gcc -O2 -o aragorn aragorn*.c
