5c91c4be6543d5234fd27e8acdfd9df04d9f2abe
Sascha Steinbiss <ss34@sanger.ac.uk>
2015-02-17 12:47:45 +0000

    remove old scripts

--- run_docker_mac.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-export ROOTDIR=`pwd`
-nextflow run -c loc_docker.config -c params_default.config annot.nf -resume -with-docker satta/annot-nf
--- run_no_resume.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-export ROOTDIR=`pwd`
-NXF_WORK=/lustre/scratch109/sanger/ss34/nxf-tmp NXF_TEMP=/lustre/scratch109/sanger/ss34/nxf-tmp ../nextflow run annot.nf
