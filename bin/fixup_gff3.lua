39bef03288910f54e923363a6d9271f22d76a475
Sascha Steinbiss <ss34@sanger.ac.uk>
2015-02-17 17:20:35 +0000

    add new scripts

---
+++ bin/fixup_gff3.lua
@@ -0,0 +1,451 @@
+#!/usr/bin/env gt
+
+--[[
+  Copyright (c) 2014-2015 Sascha Steinbiss <ss34@sanger.ac.uk>
+  Copyright (c) 2014-2015 Genome Research Ltd
+
+  Permission to use, copy, modify, and distribute this software for any
+  purpose with or without fee is hereby granted, provided that the above
+  copyright notice and this permission notice appear in all copies.
+
+  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+]]
+
+function usage()
+  io.stderr:write("Transforms a GFF3 file for loading into TriTrypDB.\n")
+  io.stderr:write(string.format("Usage: %s <GFF annotation>\n" , arg[0]))
+  os.exit(1)
+end
+
+if #arg < 1 then
+  usage()
+end
+
+function gff3_encode(s)
+  s = string.gsub(s, "[\t\n\r;=%&,]", function (c)
+        return string.format("%%%02X", string.byte(c))
+      end)
+  return s
+end
+
+function gff3_decode(s)
+  s = string.gsub(s, "%%([0-9a-fA-F][1-9a-fA-F])", function (n)
+        return string.char(tonumber("0x" .. n))
+      end)
+  return s
+end
+
+function gff3_extract_structure(str)
+  local ret = {}
+  for _,v in ipairs(split(str, ", ?")) do
+    local res = {}
+    local v = gff3_decode(v)
+    for _,pair in ipairs(split(v, ";")) do
+      if string.len(pair) > 0 then
+        local key, value = unpack(split(pair, "="))
+        res[key] = value
+      end
+    end
+    table.insert(ret, res)
+  end
+  return ret
+end
+
+function gff3_explode(tab)
+  local ret = {}
+  for _,item in ipairs(tab) do
+    local _tmp = {}
+    for k,v in pairs(item) do
+      table.insert(_tmp, k .. "=" .. v)
+    end
+    table.insert(ret, gff3_encode(table.concat(_tmp, ";")))
+  end
+  return table.concat(ret, ",")
+end
+
+function table_count(tt, value)
+  local count = 0
+  for ii,xx in pairs(tt) do
+    if xx == value then count = count + 1 end
+  end
+  return count
+end
+
+function table_unique(tt)
+  local newtable = {}
+  for ii,xx in ipairs(tt) do
+    if table_count(newtable, xx) == 0 then
+      newtable[#newtable+1] = xx
+    end
+  end
+  return newtable
+end
+
+function table_add(t1,t2)
+    for i=1,#t2 do
+        t1[#t1+1] = t2[i]
+    end
+    return t1
+end
+
+function split(str, pat)
+   local t = {}
+   local fpat = "(.-)" .. pat
+   local last_end = 1
+   local s, e, cap = str:find(fpat, 1)
+   while s do
+      if s ~= 1 or cap ~= "" then
+        table.insert(t,cap)
+      end
+      last_end = e+1
+      s, e, cap = str:find(fpat, last_end)
+   end
+   if last_end <= #str then
+      cap = str:sub(last_end)
+      table.insert(t, cap)
+   end
+   return t
+end
+
+partial_visitor = gt.custom_visitor_new()
+function partial_visitor:visit_feature(fn)
+  if fn:get_type() == 'gene' then
+    if fn:get_attribute("Start_range") then
+      if fn:get_strand() == "-" then
+        fn:set_attribute("threeEndPartial", "1")
+      else
+        fn:set_attribute("fiveEndPartial", "1")
+      end
+    end
+    if fn:get_attribute("End_range") then
+      if fn:get_strand() == "+" then
+        fn:set_attribute("threeEndPartial", "1")
+      else
+        fn:set_attribute("fiveEndPartial", "1")
+      end
+    end
+  end
+  return 0
+end
+
+seleno_tag_visitor = gt.custom_visitor_new()
+function seleno_tag_visitor:visit_feature(fn)
+  if fn:get_type() == 'polypeptide' then
+    if fn:get_attribute("product")
+      and string.match(fn:get_attribute("product"), "selenoprotein") then
+      -- we have a selenoprotein
+      if not fn:get_attribute("stop_codon_redefined_as_selenocysteine") then
+        fn:add_attribute("stop_codon_redefined_as_selenocysteine", "1")
+      end
+    end
+  end
+  return 0
+end
+
+ncrna_visitor = gt.custom_visitor_new()
+function ncrna_visitor:visit_feature(fn)
+  for fn2 in fn:get_children() do
+    if fn2:get_type() == 'ncRNA' or
+         fn2:get_type() == 'snRNA' or
+         fn2:get_type() == 'snoRNA' or
+         fn2:get_type() == 'rRNA' or
+         fn2:get_type() == 'lncRNA' or
+         fn2:get_type() == 'snRNA' or
+         fn2:get_type() == 'scRNA' or
+         fn2:get_type() == 'tRNA' then
+      for n in fn2:get_children() do
+        if n:get_type() == "CDS" or n:get_type():match("UTR") then
+          if n:get_attribute("Dbxref") and not fn2:get_attribute("Dbxref") then
+            -- copy over Dbxref from CDS to ncRNA
+            fn2:add_attribute("Dbxref", n:get_attribute("Dbxref"))
+          end
+          if n:get_attribute("product") and not fn2:get_attribute("product") then
+            -- copy over product from CDS to ncRNA
+            fn2:add_attribute("product", n:get_attribute("product"))
+          end
+          -- remove CDS from ncRNA
+          fn:remove_leaf(n)
+        end
+      end
+      if fn2:get_attribute("score") then
+        fn2:remove_attribute("score")
+      end
+      if fn2:get_attribute("model_range") then
+        fn2:remove_attribute("model_range")
+      end
+      if fn2:get_attribute("model_name") then
+        fn2:remove_attribute("model_name")
+      end
+      if fn2:get_attribute("evalue") then
+        fn2:remove_attribute("evalue")
+      end
+      if fn2:get_attribute("anticodon") then
+        fn2:remove_attribute("anticodon")
+      end
+      if fn2:get_attribute("gc_content") then
+        fn2:remove_attribute("gc_content")
+      end
+      if fn2:get_attribute("gc") then
+        fn2:remove_attribute("gc")
+      end
+      if fn2:get_attribute("aa") then
+        fn2:remove_attribute("aa")
+      end
+    end
+  end
+  return 0
+end
+
+quotes_visitor = gt.custom_visitor_new()
+function quotes_visitor:visit_feature(fn)
+  for fn2 in fn:get_children() do
+    for k,v in fn2:attribute_pairs() do
+      if v:match('"') then
+        v = v:gsub('"','')
+        fn2:set_attribute(k, v)
+      end
+    end
+  end
+  return 0
+end
+
+uc_visitor = gt.custom_visitor_new()
+function uc_visitor:visit_feature(fn)
+  ucs = {}
+  if not fn:get_attribute("tritryp_uc") then
+    for n in fn:get_children() do
+      hist = n:get_attribute("history")
+      if hist then
+        for m in hist:gmatch('[Tt][Rr][Ii][Tt][Rr][Yy][Pp]_[Uu][Cc]:"?([^;%%"]+)') do
+          table.insert(ucs, m)
+        end
+        for m in hist:gmatch('[Tt][Rr][Ii][Tt][Rr][Yy][Pp]_[Uu][Cc]%%3."?([^;%%"]+)') do
+          table.insert(ucs, m)
+        end
+        fn:remove_attribute("history")
+      end
+    end
+    ucs = table_unique(ucs)
+    if #ucs > 0 then
+      fn:add_attribute("tritryp_uc", table.concat(ucs, ","))
+    end
+  end
+  return 0
+end
+
+
+attrs_to_remove = {"translation",
+                   "controlled_curation",
+                   "gPI_anchor_cleavage_site",
+                   "orthologous_to",
+                   "ortholog_cluster",
+                   "comment",
+                   "membrane_structure",
+                   "polypeptide_domain",
+                   "cytoplasmic_polypeptide_region",
+                   "non_cytoplasmic_polypeptide_region",
+                   "transmembrane_polypeptide_region",
+                   "signal_peptide",
+                   "paralogous_to",
+                   "timelastmodified",
+                   "full_GO",
+                   "colour",
+                   "feature_id",
+                   "isObsolete"}
+remove_attrs_visitor = gt.custom_visitor_new()
+function remove_attrs_visitor:visit_feature(fn)
+  for n in fn:get_children() do
+    for _,v in ipairs(attrs_to_remove) do
+      if n:get_attribute(v) then
+        n:remove_attribute(v)
+      end
+    end
+  end
+  return 0
+end
+
+rank_fixer_visitor = gt.custom_visitor_new()
+function rank_fixer_visitor:visit_feature(fn)
+  for n in fn:get_children() do
+    local p = n:get_attribute("product")
+    if p then
+      local products = gff3_extract_structure(p)
+      local ranked = {}
+      local seen_preferred = false
+      -- gather product names
+      for _,v in ipairs(products) do
+        table.insert(ranked, v)
+      end
+      local s = n:get_attribute("product_synonym")
+      if s then
+        local syns = split(s, ",")
+        for _,v in ipairs(syns) do
+          table.insert(ranked, {term=v, rank=1})
+        end
+        n:remove_attribute("product_synonym")
+      end
+      s = n:get_attribute("synonym")
+      if s then
+        local syns = split(s, ",")
+        for _,v in ipairs(syns) do
+          table.insert(ranked, {term=v, rank=1})
+        end
+        n:remove_attribute("synonym")
+      end
+      -- assign "is_preferred" flag
+      if #ranked > 1 then
+        for _,v in ipairs(ranked) do
+          if not rank and not seen_preferred then
+            v.rank = nil
+            v.is_preferred = "true"
+            seen_preferred = true
+          else
+            v.rank = 1
+            v.is_preferred = nil
+          end
+        end
+      end
+      n:set_attribute("product", gff3_explode(ranked))
+    end
+  end
+  return 0
+end
+
+synonym_mover_visitor = gt.custom_visitor_new()
+function synonym_mover_visitor:visit_feature(fn)
+  if fn:get_type() == "gene" or fn:get_type() == "pseudogene" then
+    for fn2 in fn:get_children() do
+      if fn2:get_type():match("RNA")
+          or fn2:get_type():match("transcript")
+          or fn2:get_type():match("CDS") then
+        local syn = fn2:get_attribute("synonym")
+        if syn then
+          if not fn:get_attribute("synonym") then
+            fn:set_attribute("synonym", syn)
+          end
+          fn2:remove_attribute("synonym")
+        end
+      end
+    end
+  end
+  return 0
+end
+
+uc_getter_visitor = gt.custom_visitor_new()
+ucs = {}
+function uc_getter_visitor:visit_feature(fn)
+  local target = nil
+  if fn:get_type() ~= "polypeptide" then
+    target = fn:get_attribute("ID")
+  else
+    target = fn:get_attribute("Derives_from")
+  end
+  for fn2 in fn:get_children() do
+    local uc = fn2:get_attribute("tritryp_uc")
+    if uc then
+      if not ucs[target] then
+        ucs[target] = {}
+      end
+      for _,v in ipairs(split(uc, ",")) do
+        table.insert(ucs[target], v)
+      end
+      ucs[target] = table_unique(ucs[target])
+    end
+  end
+  return 0
+end
+
+uc_mover_visitor = gt.custom_visitor_new()
+function uc_mover_visitor:visit_feature(fn)
+  local my_ucs = {}
+  for fn2 in fn:get_children() do
+    if fn2:get_attribute("ID") and ucs[fn2:get_attribute("ID")] then
+      my_ucs = table_add(my_ucs, ucs[fn2:get_attribute("ID")])
+    end
+    if fn2:get_attribute("tritryp_uc") then
+      fn2:remove_attribute("tritryp_uc")
+    end
+  end
+  if #my_ucs > 0 then
+    table.sort(my_ucs)
+    fn:add_attribute("tritryp_uc", table.concat(my_ucs, ","))
+  end
+  return 0
+end
+
+polypeptide_child_trimmer_visitor = gt.custom_visitor_new()
+function polypeptide_child_trimmer_visitor:visit_feature(fn)
+  if fn:get_type() == "polypeptide" then
+    for fn2 in fn:direct_children() do
+      if fn2:get_type() == "membrane_structure" then
+        for fn3 in fn2:direct_children() do
+          fn:remove_leaf(fn3)
+        end
+      end
+      fn:remove_leaf(fn2)
+    end
+  end
+  return 0
+end
+
+exon_remover_visitor = gt.custom_visitor_new()
+function exon_remover_visitor:visit_feature(fn)
+  if fn:get_type() == "gene" then
+    for fn2 in fn:children() do
+      if fn2:get_type() == "exon" then
+        fn:remove_leaf(fn2)
+      end
+    end
+  end
+  return 0
+end
+
+-- set up streams
+
+gather_stream = gt.custom_stream_new_unsorted()
+gather_stream.instream = gt.gff3_in_stream_new_sorted(arg[1])
+function gather_stream:next_tree()
+  local node = self.instream:next_tree()
+  if node then
+    node:accept(uc_getter_visitor)
+  end
+  return node
+end
+
+fixup_stream = gt.custom_stream_new_unsorted()
+fixup_stream.instream = gt.gff3_in_stream_new_sorted(arg[1])
+function fixup_stream:next_tree()
+  local node = self.instream:next_tree()
+  if node then
+    node:accept(partial_visitor)
+    node:accept(seleno_tag_visitor)
+    node:accept(ncrna_visitor)
+    node:accept(quotes_visitor)
+    node:accept(remove_attrs_visitor)
+ --   node:accept(rank_fixer_visitor)
+    node:accept(synonym_mover_visitor)
+    node:accept(uc_mover_visitor)
+    node:accept(polypeptide_child_trimmer_visitor)
+    node:accept(exon_remover_visitor)
+  end
+  return node
+end
+
+-- run streams
+
+local gn = gather_stream:next_tree()
+while (gn) do
+  gn = gather_stream:next_tree()
+end
+
+out_stream = gt.gff3_out_stream_new(fixup_stream)
+local gn = out_stream:next_tree()
+while (gn) do
+  gn = out_stream:next_tree()
+end
---
+++ bin/no_abacas_prepare.lua
@@ -0,0 +1,128 @@
+#!/usr/bin/env gt
+
+--[[
+  Copyright (c) 2015 Sascha Steinbiss <ss34@sanger.ac.uk>
+  Copyright (c) 2015 Genome Research Ltd
+
+  Permission to use, copy, modify, and distribute this software for any
+  purpose with or without fee is hereby granted, provided that the above
+  copyright notice and this permission notice appear in all copies.
+
+  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+]]
+
+function usage()
+  io.stderr:write("Creates a set of input files without ABACAS input.\n")
+  io.stderr:write(string.format("Usage: %s <file> <outfileprefix>\n" , arg[0]))
+  os.exit(1)
+end
+
+if #arg ~= 2 then
+  usage()
+end
+
+seqfile = arg[1]
+outfileprefix = arg[2]
+
+package.path = gt.script_dir .. "/?.lua;" .. package.path
+require("lib")
+
+-- scaffold datastructures
+scafs = {}
+
+start = 1
+keys, seqs = get_fasta_nosep(arg[1])
+for hdr, seq in pairs(seqs) do
+  if not scafs[hdr] then
+    scafs[hdr] = {}
+  end
+  local seqlen = string.len(seq)
+  this_scaf = {start = start,
+               stop = start + seqlen - 1,
+               seq = string.sub(seq, start, stop),
+               contigs = {}}
+--print(hdr .. " " .. this_scaf.start .. " " .. this_scaf.stop )
+  local i_end = 0
+  -- gather all 'contigs'
+  while true do
+    i_start, i_end = string.find(this_scaf.seq, "[^Nn]+", i_end + 1)
+    if i_start == nil then break end
+    this_contig = {start = i_start,
+                   stop = i_end,
+                   seq = string.sub(this_scaf.seq, i_start, i_end)}
+--print("    " .. this_contig.start .. " " .. this_contig.stop )
+    -- sanity check
+    if this_contig.seq:match("[Nn]") then
+      io.stderr:write("contig with N character encountered")
+      os.exit(1)
+    end
+    table.insert(this_scaf.contigs, this_contig)
+  end
+  table.insert(scafs[hdr], this_scaf)
+end
+
+-- open files
+pseudochr_fasta_out = io.open(outfileprefix .. ".pseudochr.fasta", "w+")
+scaf_fasta_out = io.open(outfileprefix .. ".scafs.fasta", "w+")
+scaf_agp_out = io.open(outfileprefix .. ".pseudochr.agp", "w+")
+scaf_agp_out:write("##agp-version\t2.0\n")
+ctg_fasta_out = io.open(outfileprefix .. ".contigs.fasta", "w+")
+ctg_agp_out = io.open(outfileprefix .. ".scafs.agp", "w+")
+ctg_agp_out:write("##agp-version\t2.0\n")
+
+-- do the output
+scaf_i = 1
+contig_i = 1
+table.sort(keys)
+-- for all toplevel seqs...
+for _,seqid in ipairs(keys) do
+  pseudochr_fasta_out:write(">" .. seqid .. "\n")
+  print_max_width(seqs[seqid], pseudochr_fasta_out, 60)
+  print(seqid .. ":  " .. #scafs[seqid])
+  local i = 1
+  local s_last_stop = 0
+  for _, s in ipairs(scafs[seqid]) do
+    local scafname = seqid
+    scaf_fasta_out:write(">" .. scafname .. "\n")
+    print_max_width(s.seq, scaf_fasta_out, 60)
+    if s_last_stop > 0 then
+      scaf_agp_out:write(seqid .. "\t" .. tonumber(s_last_stop)+1 .. "\t"
+                        .. tonumber(s.start)-1 .. "\t" .. i .. "\tU\t"
+                        .. (tonumber(s.start)-1)-(tonumber(s_last_stop)+1) + 1
+                        .. "\tcontig\tno\talign_xgenus\n")
+      i = i + 1
+    end
+    scaf_agp_out:write(seqid .. "\t" .. s.start .. "\t"
+                       .. s.stop .. "\t" .. i .. "\tF\t" .. scafname
+                       .. "\t1\t" .. string.len(s.seq) .. "\t+\n")
+    scaf_i = scaf_i + 1
+    local j = 1
+    local c_last_stop = 0
+    for _, c in ipairs(s.contigs) do
+      local ctgname = seqid .. "_CTG" .. string.format("%06d", contig_i)
+      ctg_fasta_out:write(">" .. ctgname .. "\n")
+      print_max_width(c.seq, ctg_fasta_out, 60)
+      if c_last_stop > 0 then
+        ctg_agp_out:write(scafname .. "\t" .. tonumber(c_last_stop)+1 .. "\t"
+                          .. tonumber(c.start)-1 .. "\t" .. j .. "\tN\t"
+                          .. (tonumber(c.start)-1)-(tonumber(c_last_stop)+1) + 1
+                          .. "\tscaffold\tyes\tunspecified\n")
+        j = j + 1
+      end
+      ctg_agp_out:write(scafname .. "\t" .. c.start .. "\t"
+                        .. c.stop .. "\t" .. j .. "\tF\t" .. ctgname
+                        .. "\t1\t" .. string.len(c.seq) .. "\t+\n")
+      contig_i = contig_i + 1
+      j = j + 1
+      c_last_stop = c.stop
+    end
+    i = i + 1
+    s_last_stop = tonumber(s.stop)
+  end
+end
