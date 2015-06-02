3955e93dd350fc105fbb0e3036041fece975b1bd
Sascha Steinbiss <ss34@sanger.ac.uk>
2015-06-02 15:30:44 +0100

    compile own copy of LAST for Travis

---
+++ test/travis.setup-last.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -ev
+rm -f last-581.zip
+wget -q http://last.cbrc.jp/last-581.zip
+unzip last-581.zip
+cd last-581
+make -j3
+make install
+cd ..
+rm -rf last-581
+rm -f last-581.zip
