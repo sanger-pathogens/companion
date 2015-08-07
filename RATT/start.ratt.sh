0c689ee4cfc8cdb164f283784d514a8ffb36efb6
Sascha Steinbiss <sascha@steinbiss.name>
2015-08-07 21:56:12 +0100

    do not fail on rm

--- RATT/start.ratt.sh
+++ RATT/start.ratt.sh
@@ -17,32 +17,32 @@ fi;
 
 NUCMER_PATH=$PAGIT_HOME/bin/;
 ## check the entrance
-if [ -z "$parameterSet" ]; then 
+if [ -z "$parameterSet" ]; then
 	echo "Please use RATT with the following options:
   $RATT_HOME/start.ratt.sh <Directory with embl-files> <Query-fasta sequence> <Resultname> <Transfer type> <optional: reference (multi) Fasta>
 
-  
-  Directory name with 
+
+  Directory name with
   embl-annotation files  - This directory contains all the embl files that should be transfered to the query.
   Query.fasta            - A multifasta file to, which the annotation will be mapped.
   ResultName             - The prefix you wish to give to each result file.
   Transfer type          - Following parameters can be used (see below for the different used sets)
-       (i)   Assembly:             Transfer between different assemblies. 
-       (ii)  Assembly.Repetitive:  As before, but the genome is extremely repetitive. 
-                   This should be run, only if the parameter Assembly doesn't return good results (misses too many annotation tags).  
+       (i)   Assembly:             Transfer between different assemblies.
+       (ii)  Assembly.Repetitive:  As before, but the genome is extremely repetitive.
+                   This should be run, only if the parameter Assembly doesn't return good results (misses too many annotation tags).
        (iii) Strain:              Transfer between strains. Similarity is between 95-99%.
-       (iv)  Strain.Repetitive:   As before, but the genome is extremely repetitive. 
+       (iv)  Strain.Repetitive:   As before, but the genome is extremely repetitive.
                    This should be run, only if the parameter Strain doesn't return good results (misses too many annotation tags).
        (v)   Species:              Transfer between species. Similarity is between 50-94%.
-       (vi)  Species.Repetitive:   As before, but the genome is extremely repetitive. 
+       (vi)  Species.Repetitive:   As before, but the genome is extremely repetitive.
                    This should be run, only if the parameter Species doesn't return good results (misses too many annotation tags).
        (vii) Multiple:             When many annotated strains are used as a reference, and you assume the newly sequenced genome has many insertions
-                   compared to the strains in the query (reference?). This parameter will use the best regions of each reference strain to transfer tags.    
+                   compared to the strains in the query (reference?). This parameter will use the best regions of each reference strain to transfer tags.
        (viii)Free:                 The user sets all parameter individually.
 
-  reference fasta        - Name of multi-fasta. VERY I M P O R T A N T The name of each sequence in the fasta description, 
-                   MUST be the same name as its corresponding embl file. So if your embl file is call Tuberculosis.embl, in your reference.fasta file, 
-                   the description has to be 
+  reference fasta        - Name of multi-fasta. VERY I M P O R T A N T The name of each sequence in the fasta description,
+                   MUST be the same name as its corresponding embl file. So if your embl file is call Tuberculosis.embl, in your reference.fasta file,
+                   the description has to be
                            >Tuberculsosis
                            ATTGCGTACG
                            ..."
@@ -54,7 +54,7 @@ embl-annotation files     - This directory contains all the embl files that shou
   Last output of iCORN    - The name of the reference.x - were x is the last iteration.
   ResultName              - The prefix you wish to give to each result file.
 
-" 
+"
    exit
  fi
 
@@ -65,13 +65,13 @@ fi
 
 if [ -z "$RATT_DOTRANSLATION" ] ;
 	then
-	RATT_DOTRANSLATION=0	
+	RATT_DOTRANSLATION=0
 fi
 
 ### nucmer call as function
 function doNucmer {
-	if [ ! -z "$verbose" ] 
-		then 
+	if [ ! -z "$verbose" ]
+		then
 		echo "nucmer  $other_nucmer -g $g -p $name -c $c -l $l $ref $query"
 		echo "delta-filter $rearrange -i $minInd $name.delta > $name.filter.delta"
 	fi
@@ -95,7 +95,7 @@ function doiCORN {
 	cd "$root$(($iteration-1))"
 
 	cd "$root$(($iteration-1))"
-	
+
 
 	# geneate the plot
 	mkdir plot
@@ -106,7 +106,7 @@ function doiCORN {
 	cd RATT.$iteration/Seq
 
 	perl $RATT_HOME/main.ratt.pl Split ../../$root.$iteration
-	
+
 	cd ..
 	ln -s ../$root$(($iteration-1))/plot
 
@@ -116,18 +116,18 @@ function doiCORN {
 	mkdir tmp
 	for x in `grep '>' ../$root.$iteration | sed 's/>//g' | awk '{ print $1 }'` ; do
 		cat ../$root.*.$x.gff > ../All.$x.gff;
-		egrep "\"INS|\"DEL"  ../All.$x.gff | sort -n -k 4 > tmp/All.indel.$x.gff; 
+		egrep "\"INS|\"DEL"  ../All.$x.gff | sort -n -k 4 > tmp/All.indel.$x.gff;
 		echo "perl $RATT_HOME/ratt.icorn.pl embl.$tmp/$x.embl tmp/All.indel.$x.gff $x $x.embl 4000000"
 		perl $RATT_HOME/ratt.icorn.pl  embl.$tmp/$x.embl tmp/All.indel.$x.gff $x $x.embl 4000000 > out;
 echo "perl ~/Bin/icorn.flagNonCorrectedRegions.pl Seq/$x plot/Iter.$iteration.$x.plot $x 20 100 Not+Corrected"
 		perl ~/Bin/icorn.flagNonCorrectedRegions.pl Seq/$x plot/Iter.$iteration.$x.plot $x 20 100 Not+Corrected;
-		
+
 	done;
 
 
 }
 
-if [ "$refembl" == "iCORN" ] ; then 
+if [ "$refembl" == "iCORN" ] ; then
 	refembl=$2;
 	query=$3
 	result=$4
@@ -144,12 +144,12 @@ EMBL_files=${EMBL_files-`ls $refembl/ | wc 2>/dev/null`}
 if (test "$NUCMER_EXE" == "");
 then
    echo "nucmer is not on the PATH"
-   exit; 
+   exit;
 elif(test "$EMBL_files" == "");
 then
    echo "Cannot find any EMBL files in $refembl is not on the PATH."
    echo "So far just embl formatted files can be used. They must end with .embl."
-   exit; 
+   exit;
 fi
 
 tmp=$$;
@@ -167,8 +167,8 @@ query=query.$tmp
 orig_query=$query
 
 # check for the reference
-if [ -f "$ref" ] 
-	then 
+if [ -f "$ref" ]
+	then
 	# if the reference exist, use this
 	echo "I am using the reference $ref. Please make sure that the description line of each fasta entry is the same than in the embl file name!";
 	head -n 1 $ref
@@ -180,7 +180,7 @@ else
 	ref="Reference.$tmp.fasta"
 	perl $RATT_HOME/main.ratt.pl Embl2Fasta $refembl $ref
 fi
-	
+
 ### check if files ok
 if [ ! -f "$ref" ]                # be sure the directory /mnt exists
   then
@@ -206,11 +206,11 @@ doneDifference=0;
 # minLength - minlength of a hit   -- not used so far...
 
 if [ "$parameterSet" == "Assembly" ] || [ "$parameterSet" == "Assembly.Repetitive" ] ;
-	then 
+	then
 	c=400;
 	l=30;
 	g=1000;
-	
+
 	if [ "$parameterSet" == "Assembly.Repetitive" ] ;
 		then
 		other_nucmer=" --maxmatch "
@@ -219,9 +219,9 @@ if [ "$parameterSet" == "Assembly" ] || [ "$parameterSet" == "Assembly.Repetitiv
 	fi
 	rearrange=" -g -o 0 ";
 	minInd=99;
-	
+
 	### get real SNP before mutate
-	doNucmer 
+	doNucmer
 
 	perl $RATT_HOME/main.ratt.pl Difference  $name.snp $name.filter.coords $result
 	doneDifference=1;
@@ -230,7 +230,7 @@ if [ "$parameterSet" == "Assembly" ] || [ "$parameterSet" == "Assembly.Repetitiv
 	perl $RATT_HOME/main.ratt.pl Mutate $query
 	return=$?
 	if [ "$return" != "0" ] ;
-		then 
+		then
 		echo "See Error in BBA.main script, to mutate the query for Assembly to Assembly annotation transfer."
 		exit 1;
 	fi;
@@ -268,7 +268,7 @@ elif [ "$parameterSet" == "Pacbio" ] ;
         ### update name of query
         query=$query."mutated"
 elif [ "$parameterSet" == "Falciparum" ] ||  [ "$parameterSet" == "Falciparum.Repetitive" ] ;
-	then 
+	then
 	c=400;
 	l=150;
 	g=2000;
@@ -279,10 +279,10 @@ elif [ "$parameterSet" == "Falciparum" ] ||  [ "$parameterSet" == "Falciparum.Re
 	else
 		other_nucmer="  "
 	fi
-	
+
 	rearrange=" -r -o 1 ";
 	minInd=90;
-	
+
 	### get real SNP before mutate
 #	doNucmer
 
@@ -293,7 +293,7 @@ elif [ "$parameterSet" == "Falciparum" ] ||  [ "$parameterSet" == "Falciparum.Re
 #	perl $RATT_HOME/main.ratt.pl Mutate $query
 #	return=$?
 #	if [ "$return" != "0" ] ;
-#		then 
+#		then
 #		echo "See Error in BBA.main script, to mutate the query for Assembly to Assembly annotation transfer."
 #		exit 1;
 #	fi;
@@ -301,7 +301,7 @@ elif [ "$parameterSet" == "Falciparum" ] ||  [ "$parameterSet" == "Falciparum.Re
 #	query=$query."mutated"
 
 elif [ "$parameterSet" == "Strain" ] ||  [ "$parameterSet" == "Strain.Repetitive" ] || [ "$parameterSet" == "Strain.global" ] ||  [ "$parameterSet" == "Strain.global.Repetitive" ];
-	then 
+	then
 	c=400;
 	l=20;
 	g=500;
@@ -312,16 +312,16 @@ elif [ "$parameterSet" == "Strain" ] ||  [ "$parameterSet" == "Strain.Repetitive
 	else
 		other_nucmer="  "
 	fi
-minInd=90;	
+minInd=90;
 	rearrange=" -r -o 1 ";
 	if  [ "$parameterSet" == "Strain.global" ] ||  [ "$parameterSet" == "Strain.global.Repetitive" ] ;
 		then
 		rearrange=" -1 -o 5 ";
-                minInd=98;	
+                minInd=98;
 fi
 
 
-	
+
 	### get real SNP before mutate
 	doNucmer
 
@@ -332,7 +332,7 @@ fi
 	perl $RATT_HOME/main.ratt.pl Mutate $query
 	return=$?
 	if [ "$return" != "0" ] ;
-		then 
+		then
 		echo "See Error in BBA.main script, to mutate the query for Assembly to Assembly annotation transfer."
 		exit 1;
 	fi;
@@ -340,7 +340,7 @@ fi
 	query=$query."mutated"
 
 elif [ "$parameterSet" == "Species" ]  || [ "$parameterSet" == "Species.Repetitive" ] || [ "$parameterSet" == "Species.global" ]  || [ "$parameterSet" == "Species.global.Repetitive" ] ;
-	then 
+	then
 	if [ "$parameterSet" == "Species.Repetitive" ] ;
 		then
 		other_nucmer=" --maxmatch "
@@ -352,23 +352,23 @@ elif [ "$parameterSet" == "Species" ]  || [ "$parameterSet" == "Species.Repetiti
 	l=10;
 	g=1000;
 	minInd=40;
- 
+
 	rearrange=" -r -o 5 ";
 	if  [ "$parameterSet" == "Species.global" ]  || [ "$parameterSet" == "Species.global.Repetitive" ] ;
 		then
 		rearrange=" -g -o 5 ";
 	fi
 elif [ "$parameterSet" == "Multiple" ] ;
-	then 
+	then
 	c=400;
 	l=25;
 	g=1000;
-	
+
 	other_nucmer=" --maxmatch "
 	rearrange=" -q -o 1";
 	minInd=98;
 elif [ "$parameterSet" == "Free" ] ;
-	then 
+	then
 	c=$RATT_c;
 	l=$RATT_l;
 	g=$RATT_g;
@@ -386,12 +386,12 @@ doNucmer
 
 # print the synteny stats and do the difference files.
 if [ "$doneDifference" == "0" ]
-	then 
+	then
 	perl $RATT_HOME/main.ratt.pl Difference  $name.snp $name.filter.coords $result
 fi
 
 ### do the tranfer
-if [ ! -z "$verbose" ] 
+if [ ! -z "$verbose" ]
 	then
 	echo "Nucmer is done. Now transfer the annotation."
 	echo "perl $RATT_HOME/main.ratt.pl $refembl $name.snp $name.filter.coords $result"
@@ -400,7 +400,7 @@ fi
 perl $RATT_HOME/main.ratt.pl Transfer $refembl $name.snp $name.filter.coords $result
 return=$?
 if [ "$return" != "0" ] ;
-	then 
+	then
 	echo "Sorry, the RATT transfer step did fail.\n";
 	echo "Debug information: perl $RATT_HOME/main.ratt.pl Transfer $refembl $name.snp $name.filter.coords $result";
 	pwd
@@ -415,7 +415,7 @@ ln -s $orig_query tmpSeqXXX.$tmp
 cd Sequences
 perl $RATT_HOME/main.ratt.pl Split ../tmpSeqXXX.$tmp
 cd ..
-rm tmpSeqXXX.$tmp
+rm -f tmpSeqXXX.$tmp
 
 echo "Nucmer is done. Now Correct the annotation for chromosome $nameRes."
 
@@ -426,12 +426,12 @@ for nameRes in `grep '>' $query | perl -nle 's/\|/_/g;/>(\S+)/; print $1'` ; do
 		echo "************************ Correction *****"
 		perl $RATT_HOME/main.ratt.pl Correct $result.$nameRes.embl Sequences/$nameRes $result.$nameRes
 	echo "************************ "
-	else 
-		### generate empty file for the joining 
+	else
+		### generate empty file for the joining
 		touch $result.$nameRes.tmp2.embl
 	fi
 
-	perl $RATT_HOME/main.ratt.pl doEMBL $result.$nameRes.final $result.$nameRes.tmp2.embl Sequences/$nameRes 
+	perl $RATT_HOME/main.ratt.pl doEMBL $result.$nameRes.final $result.$nameRes.tmp2.embl Sequences/$nameRes
 	if [ "$RATT_DOTRANSLATION" -eq 1 ] ; then
 		perl $RATT_HOME/main.ratt.pl addTranslation $result.$nameRes.final.embl
 	fi
@@ -440,5 +440,5 @@ for nameRes in `grep '>' $query | perl -nle 's/\|/_/g;/>(\S+)/; print $1'` ; do
 
 done
 
-rm $result*embl.tmp.BBA.embl # $result.$nameRes.tmp2.embl
+rm -f $result*embl.tmp.BBA.embl # $result.$nameRes.tmp2.embl
 
