3e788e586498eee5d3d6f3ffed6c2a97e2608e9d
Sascha Steinbiss <ss34@sanger.ac.uk>
2015-09-11 13:52:20 +0100

    do not force ABACAS to always run BLAST

--- ABACAS2/abacas2.nonparallel.sh
+++ ABACAS2/abacas2.nonparallel.sh
@@ -71,34 +71,34 @@ done
 abacas2.bin.sh $contig Res.abacasBin.fna && grep -v '>'  Res.abacasBin.fna | awk 'BEGIN {print ">Bin.union"} {print}' > Res.abacasBin.oneSeq.fna_ && cat Res*fna > Genome.abacas.fasta && bam.correctLineLength.sh Genome.abacas.fasta  &> /dev/null && mv  Res.abacasBin.fna_ Res.abacasBin.fna
 
 #~tdo/Bin/abacas2.doblast.sh $reference Res
-ref=$reference
-pre=Res
+#ef=$reference
+#pre=Res
 
-if [ -z "$pre" ] ; then
-   echo "usage <reference> Res"
+#if [ -z "$pre" ] ; then
+#   echo "usage <reference> Res"
 
-fi
+#fi
 
-tmp=$$
+#tmp=$$
 
-ln -s $ref REF.$tmp
+#ln -s $ref REF.$tmp
 
-mkdir Reference
-cd Reference
-SeperateSequences.pl  ../REF.$tmp
-cd ..
-mkdir comp
+#mkdir Reference
+#cd Reference
+#SeperateSequences.pl  ../REF.$tmp
+#cd ..
+#mkdir comp
 
-count=0
-for nameRes in `grep '>' $ref | perl -nle 's/\|/_/g;/>(\S+)/; print $1'` ; do
-	let count++;
-	if [ $count -gt 200 ] ; then
-		echo "too many contigs to bsub!\n";
-		exit
-	fi
-	formatdb -p F -i Reference/$nameRes;
+#count=0
+#for nameRes in `grep '>' $ref | perl -nle 's/\|/_/g;/>(\S+)/; print $1'` ; do
+#	let count++;
+#	if [ $count -gt 200 ] ; then
+#		echo "too many contigs to bsub!\n";
+#		exit
+#	fi
+#	formatdb -p F -i Reference/$nameRes;
 
-	megablast -F T -m 8 -e 1e-20 -d Reference/$nameRes -i $pre.$nameRes.fna -o comp/comp.$nameRes.blast
+#	megablast -F T -m 8 -e 1e-20 -d Reference/$nameRes -i $pre.$nameRes.fna -o comp/comp.$nameRes.blast
 
 #if [ -z "$Other" ] ; then
 #echo  " bsub -o blast.$nameRes.o -e blast.$nameRes.e -R \"select[type==X86_64 && mem > 6000] rusage[mem=6000]\" -M6000000  blastall -p blastn -F F -W 15 -m 8 -e 1e-20 -d Reference/$nameRes -i $pre.$nameRes.fna -o comp/comp.$nameRes.blast"
@@ -107,7 +107,7 @@ for nameRes in `grep '>' $ref | perl -nle 's/\|/_/g;/>(\S+)/; print $1'` ; do
 
  #blastall -p blastn  $Other -m 8 -e 1e-20 -d Reference/$nameRes -i $pre.$nameRes.fna -o comp/comp.$nameRes.blast
 
-done
+#done
 
 
 
