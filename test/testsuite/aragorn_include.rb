a8f7101a40d39abd0fab93a0580bd68456830cb5
Sascha Steinbiss <ss34@sanger.ac.uk>
2015-11-30 17:51:05 +0000

    fix test suite
    This addresses the fact that different GenomeTools versions output
    different GFF3 headers, hence making it more difficult to use diff.

--- test/testsuite/aragorn_include.rb
+++ test/testsuite/aragorn_include.rb
@@ -8,16 +8,17 @@ end
 Name "aragorn: aragorn_to_gff3 regular file"
 Keywords "aragorn aragorn_to_gff3"
 Test do
+  run("#{$bin}gt gff3 -sort -tidy -retainids #{$testdata}/aragorn.gff3 > ref")
   run_test("#{$bin}gt #{$bindir}/aragorn_to_gff3.lua < #{$testdata}/aragorn.out")
   run("#{$bin}gt gff3 -sort -tidy -retainids #{last_stdout} | tee gff3")
-  run("diff #{last_stdout} #{$testdata}/aragorn.gff3")
+  run("diff #{last_stdout} ref")
   run_test("#{$bin}gt gff3validator gff3")
 end
 
 Name "aragorn: aragorn_to_gff3 malformed file (incomplete lines)"
 Keywords "aragorn aragorn_to_gff3"
 Test do
-  run_test("#{$bin}gt #{$bindir}/aragorn_to_gff3.lua< #{$testdata}/aragorn.malformed.out", :retval => 1)
+  run_test("#{$bin}gt #{$bindir}/aragorn_to_gff3.lua < #{$testdata}/aragorn.malformed.out", :retval => 1)
 end
 
 Name "aragorn: aragorn_to_gff3 malformed file (no seqs)"
